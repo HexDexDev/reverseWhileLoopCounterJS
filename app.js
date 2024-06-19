ec262aa9cfdcdc7cefcd9afba8f5e2a644c7065c
HexDexDev <138143730+HexDexDev@users.noreply.github.com>
2024-06-19 19:41:41 -0300

    Adiciona README.md e app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+// Cria o contador que começa com 10
+let contador = 10;
+
+// Loop while que continua enquanto contador for maior que 0
+while (contador >= 0) {
+    // Mostra o valor atual do contador
+    alert(contador);
+    // Reduz o contador
+    contador--;
+}
